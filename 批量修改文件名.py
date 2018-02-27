eeea52102fd7c08d5384c721f9e6939bbe5a336c
Denis <34310801+Oslomayor@users.noreply.github.com>
2018-02-28 00:01:35 +0800

    Add files via upload

---
+++ 批量修改文件名.py
@@ -0,0 +1,13 @@
+# Feb, 27th, 2018 @ dormitory 5# R627
+# 批量修改文件名
+import os
+path = 'E:/Don\'t let language limit your world/TomatoEnglish/test'
+count = 0
+# os.listdir(path) 返回一个文件名列表
+for file_name in os.listdir(path):
+    # 切片获取文件扩展名
+    if file_name[-3::] == 'txt':
+        count += 1
+        new_name = str(count) + '.txt'
+        # os.rename 的参数要写全文件路径
+        os.rename(path + '/' + file_name,path + '/' + new_name)
